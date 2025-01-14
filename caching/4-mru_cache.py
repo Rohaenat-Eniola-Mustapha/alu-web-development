75942d896d58d9ef635431698f01149d2585e415
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-14 08:51:30 +0200

    MRU Caching

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" MRU Caching Module """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """ MRU Cache System """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.cache_data = OrderedDict()  # Use OrderedDict to maintain MRU order
+
+    def put(self, key, item):
+        """
+        Add an item in the cache using MRU policy.
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key, _ = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key from the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
