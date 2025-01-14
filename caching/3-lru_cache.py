40a264ce3ef4ea78bd044ffbee70e947cbd4aa0f
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-14 08:32:42 +0200

    LRU Caching

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" LRU Caching Module """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU Cache System """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """
+        Add an item in the cache using LRU policy.
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            del self.cache_data[key]
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key from the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
