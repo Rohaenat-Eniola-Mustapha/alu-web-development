5a71f0d71edfb0655b5582e1fb639efdfd2f8f24
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-23 09:29:30 +0200

    task 1

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,10 +9,12 @@ from api.v1.views.index import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.errorhandler(401)
 def unauthorized_error(error):
     """Handles 401 Unauthorized errors"""
     return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -6,10 +6,12 @@ from flask import Blueprint, jsonify, abort
 
 app_views = Blueprint('app_views', __name__)
 
+
 @app_views.route('/api/v1/status', methods=['GET'])
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized():
