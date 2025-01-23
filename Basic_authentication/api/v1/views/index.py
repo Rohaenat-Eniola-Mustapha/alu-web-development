e3c668ba06dd9fcec4f15057139ecaf54796fdd5
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-23 09:04:03 +0200

    task 1

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -9,4 +9,5 @@ app_views = Blueprint('app_views', __name__)
 @app_views.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized():
     """Endpoint to trigger 401 Unauthorized error"""
+
     abort(401)
