63acd0c37077025ee4c681e806ab173292a2180a
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-24 20:43:18 +0200

    Complete Tas 7-8

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -60,3 +60,18 @@ class SessionAuth(Auth):
             return User.get(user_id)
         except Exception:
             return None
+
+    def destroy_session(self, request=None):
+        """
+        Deletes the user session / logout.
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
