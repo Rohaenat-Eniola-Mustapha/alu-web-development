88c66dde9a28384baa8698cfb9e26b589d2ab621
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-24 20:24:56 +0200

    Complete Task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -83,11 +83,13 @@ def before_request() -> None:
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/',
     ]
     if not auth.require_auth(path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    # Check for both authorization header and session cookie
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
