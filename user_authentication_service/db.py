1cc77bc3ca8c04560047e2256d4502242e2b4582
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-27 10:12:34 +0200

    task 1

---
+++ user_authentication_service/db.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+DB class to interact with the database.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from user import Base, User
+
+
+class DB:
+    def __init__(self):
+        """
+        Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """
+        Creates and returns a database session.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The created user instance.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
