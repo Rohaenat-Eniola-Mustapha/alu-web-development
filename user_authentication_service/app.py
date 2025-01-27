4469fbd7700d6a9cd036c4a2bf234240e69c3e5e
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-27 12:12:36 +0200

    task 7

---
+++ user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app.
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    """
+    GET route for the home page.
+    Returns a JSON payload with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
