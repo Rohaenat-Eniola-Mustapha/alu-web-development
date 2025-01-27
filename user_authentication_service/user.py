aee1a665aa77ed003fd8ad0b2aaa4eb6d2ea7a46
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-27 10:08:07 +0200

    redo

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python3
 """
-User file.
+SQLAlchemy User model for the users table.
 """
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-def User(Base):
+class User(Base):
     """
-    Defining any number of mapped classes.
-
-    Args:
-        Base: Mapped classes.
-
-    Returns:
-        A database.
+    User model for the users table.
     """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
-    def __repr__(self):
-        return  "User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
-            self.email, self.hashed_password, self.session-id, self.reset_token
-        )
-        
-    return
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
